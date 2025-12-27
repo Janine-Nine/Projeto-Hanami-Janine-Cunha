399e557edcc2418c8f04774b0dbaf0c5234cc3c9
Janine <janinecunha213@gmail.com>
2025-12-27 00:06:21 -0300

    Delete routes directory

--- routes/reports.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { salesSummary } from './reports.controller'
-
-const router = Router()
-
-router.get('/sales-summary', salesSummary)
-
-export default router
--- routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { getReports } from "./controllers/report.controller"
-
-const router = Router()
-
-router.get("/reports", getReports)
-
-export default router
