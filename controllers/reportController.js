4c62e0d4ad965c9b7572e05036d88cc59dfa0427
Janine <janinecunha213@gmail.com>
2025-12-27 00:04:46 -0300

    Delete controllers directory

--- controllers/reportController.js
+++
@@ -1,9 +0,0 @@
-import { getSalesSummary, getProductAnalysis } from '../services/reportService.js';
-
-export async function salesSummary(req, res) {
-  res.json(await getSalesSummary());
-}
-
-export async function productAnalysis(req, res) {
-  res.json(await getProductAnalysis());
-}
